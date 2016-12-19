aec2c3bb59fe6bf9463c9aa77901a4a34feaddb2
Jonathan Ogbimi <Jonathan Ogbimi>
2016-12-19 12:26:00 +0100

    first commit

---
+++ client.js
@@ -0,0 +1,32 @@
+var net = require('net-socket');
+
+var client = new net.Socket();
+client.connect(9000, '127.0.0.1', function () {
+    console.log('Connected');
+    client.write('Hello, server! Love, Client.');
+});
+
+client.on('data', function (data) {
+    console.log('Received: ' + data);
+    client.destroy(); // kill client after server's response
+});
+
+client.on('close', function () {
+    console.log('Connection closed');
+});
+
+var net = require('net-socket');
+ 
+var socket = net.connect(9000, 'localhost');
+ 
+socket.setEncoding('utf8');
+socket.on('connect', function () {
+    // connected 
+    
+    socket.end('hey');
+    socket.destroy();
+});
+
+function handleConnection(){
+    
+}
